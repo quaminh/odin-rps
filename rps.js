bf607b5cc3f0eba0d43b3b1065e4e9607c3c3af1
Quamin Hua <quaminhua56@gmail.com>
2025-02-09 16:09:17 -0800

    Implement full functionality of game
    Was too focused coding to commit more, oops

--- rps.js
+++ rps.js
@@ -12,12 +12,68 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let play = prompt("Play 'rock'|'paper'|'scissors': ")
+    let play = "";
+    while (play !== "rock" && play !== "paper" && play !== "scissors") {
+        play = prompt("Play 'rock'|'paper'|'scissors'")
+        play = play.toLowerCase()
+    }
     return play;
 }
 
-for (let i = 0; i < 10; ++i) {
-    console.log(getComputerChoice())
+function playRound(humanChoice, computerChoice) {
+    let outcome = 0 // 0: Lose, 1: Win, 2: Draw
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                outcome = 2
+                break
+            case "scissors":
+                outcome = 1
+        }
+    }
+    else if (humanChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                outcome = 1
+                break
+            case "paper":
+                outcome = 2
+        }
+    }
+    else if (humanChoice === "scissors") {
+        switch (computerChoice) {
+            case "paper":
+                outcome = 1
+                break
+            case "scissors":
+                outcome = 2
+        }
+    }
+
+    if (outcome === 0) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+    }
+    else if (outcome === 1) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    }
+    else {
+        console.log("Draw!")
+    }
+    return outcome
 }
 
-getHumanChoice()
+let humanScore = 0
+let computerScore = 0
+
+for (let i = 1; i <= 5; ++i) {
+    console.log(`Round ${i}:`)
+    let result = playRound(getHumanChoice(), getComputerChoice())
+    if (result === 0) {
+        ++computerScore
+    }
+    else if (result === 1) {
+        ++humanScore
+    }
+}
+
+console.log(`Final Scores:\n Player: ${humanScore}\n Computer: ${computerScore}`)
