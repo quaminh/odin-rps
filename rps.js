b2e75f4769185065955f816ef69f7281cad558cd
Quamin Hua <quaminhua56@gmail.com>
2025-02-10 18:28:23 -0800

    Rework game logic to work around event listeners

--- rps.js
+++ rps.js
@@ -11,51 +11,55 @@ function getComputerChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    let outcome = 0 // 0: Lose, 1: Win, 2: Draw
-    if (humanChoice === "rock") {
-        switch (computerChoice) {
-            case "rock":
-                outcome = 2
-                break
-            case "scissors":
-                outcome = 1
-        }
-    }
-    else if (humanChoice === "paper") {
-        switch (computerChoice) {
-            case "rock":
-                outcome = 1
-                break
-            case "paper":
-                outcome = 2
-        }
-    }
-    else if (humanChoice === "scissors") {
-        switch (computerChoice) {
-            case "paper":
-                outcome = 1
-                break
-            case "scissors":
-                outcome = 2
-        }
-    }
-
-    if (outcome === 0) {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-    }
-    else if (outcome === 1) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-    }
-    else {
-        console.log("Draw!")
-    }
-    return outcome
-}
-
-
-
 let humanScore = 0
 let computerScore = 0
 
-console.log(`Final Scores:\n Player: ${humanScore}\n Computer: ${computerScore}`)
+const resultText = document.querySelector('.result');
+const scoreText = document.querySelector('.score');
+
+function playRound(event) {
+    let playerChoice = event.target.id;
+    let computerChoice = getComputerChoice();
+    let outcome = 0; // 0: lose, 1: win, 2: draw
+    switch (playerChoice) {
+        case 'rock':
+            switch (computerChoice) {
+                case 'scissors':
+                    outcome = 1;
+                    break;
+                case 'rock':
+                    outcome = 2;
+            }
+            break;
+        case 'paper':
+            switch (computerChoice) {
+                case 'rock':
+                    outcome = 1;
+                    break;
+                case 'paper':
+                    outcome = 2;
+            }
+            break;
+        case 'scissors':
+            switch (computerChoice) {
+                case 'paper':
+                    outcome = 1;
+                    break;
+                case 'scissors':
+                    outcome = 2;
+            }
+    }
+    switch (outcome) {
+        case 0:
+            ++computerScore;
+            resultText.textContent = `You lose! ${computerChoice} beats ${playerChoice}`
+            break;
+        case 1:
+            ++playerScore;
+            resultText.textContent = `You win! ${playerChoice} beats ${computerChoice}`
+            break;
+        case 2:
+            resultText.textContent = "Draw!";
+    }
+    scoreText.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+}
